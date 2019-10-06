21ce6382c0344db7d58041e5b2322c720105e432
Keseru Szabolcs <keserusz94@gmail.com>
2019-10-06 21:50:51 +0300

    Method overloadin

--- src/main/java/org/FastTrackIT/App.java
+++ src/main/java/org/FastTrackIT/App.java
@@ -1,5 +1,7 @@
 package org.FastTrackIT;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("First number? ");
+        //double a = scanner.nextDouble();
+        //System.out.println("Second number? ");
+        //double b = scanner.nextDouble();
+        //System.out.println( "Hello World!" );
+        Calculate calculate = new Calculate();
+        calculate.add(1.1, 1.1);
+        System.out.println(calculate.result);
+        calculate.multiply(1.1,1.1);
+        System.out.println(calculate.result);
+        calculate.divide(1.1,1.1);
+        System.out.println(calculate.result);
+
     }
 }
---
+++ src/main/java/org/FastTrackIT/Calculate.java
@@ -0,0 +1,56 @@
+package org.FastTrackIT;
+
+public class Calculate {
+
+
+    double result;
+
+    public double add(int a, int b){
+        result = a + b;
+        return result;
+    }
+    public double add(double a, int b){
+        result = a + b;
+        return result;
+    }
+    public double add(int a, double b){
+        result = a + b;
+        return result;
+    }
+    public double add(double a, double b){
+        result = a + b;
+        return result;
+    }
+    public double multiply(int a, int b){
+        result = a * b;
+        return result;
+    }
+    public double multiply(double a, int b){
+        result = a * b;
+        return result;
+    }
+    public double multiply(int a, double b){
+        result = a * b;
+        return result;
+    }
+    public double multiply(double a, double b){
+        result = a * b;
+        return result;
+    }
+    public double divide(int a, int b){
+        result = a / b;
+        return result;
+    }
+    public double divide(double a, int b){
+        result = a / b;
+        return result;
+    }
+    public double divide(int a, double b){
+        result = a / b;
+        return result;
+    }
+    public double divide(double a, double b){
+        result = a / b;
+        return result;
+    }
+}
